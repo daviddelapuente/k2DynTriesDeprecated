add569473fc342fe00a237f0f451dedaa84191cf
daviddelapuente <you@example.com>
2021-02-03 15:11:53 -0300

    some refactor

--- tests/otherTests.c
+++ tests/otherTests.c
@@ -1,11 +1,12 @@
 #include "../materializeSuite.c"
 #include "assert.h"
 
+//this function will test the materialization of a Trie
 void testMaterializeTrie(){
+    //first we instanciate the trie
     trieNode *t = (trieNode *) malloc(sizeof(trieNode));
     t->children[0] = t->children[1] = t->children[2] = t->children[3] = NULL;
     t->block = NULL;
-
     double alpha = 0.99;
     N1 = 4;
     Nt = S3;
@@ -14,24 +15,16 @@ void testMaterializeTrie(){
         if (i > N1) N1 = 4*(((uint16_t)ceil((double)N1/alpha)+3)/4);
         sizeArray[i] = N1;
     }
-
-    uint64_t n, n1;
-    n=7414866;
-    n1=7414866;
-
+    //now we inert the first 65536 points (in fourth base)
     long long c=0;
-
     while(c<65536){
-
-
         uint8_t path[30]={1,2,3,0,1,0,0,0,0,0,0,0,0,0,0,(uint8_t) ((c/16384)%4),(uint8_t) ((c/4096)%4),(uint8_t) ((c/1024)%4),(uint8_t) ((c/256)%4),(uint8_t) ((c/64)%4),(uint8_t) ((c/16)%4),(uint8_t) ((c/4)%4),(uint8_t) (c%4)};
         insertTrie(t, path, 23, 22);
-
         c++;
     }
 
-    char srcFolder[] = "../materializeField/";
-
+    //now we materialize the trie in ../materializeField/A1/
+    char srcFolder[] = "../materializeField/A1/";
     materializeTrie(t,srcFolder);
 }
 
